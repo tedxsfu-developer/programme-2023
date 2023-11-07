f4438bc99b1e6a592003d234cb0f08db86e91e28
Kelly <75859041+kellyhady@users.noreply.github.com>
2023-11-07 13:52:57 -0800

    updates
    updated ProgramItems.tsx
    updated index

--- src/components/ProgramItem.tsx
+++ src/components/ProgramItem.tsx
@@ -10,18 +10,31 @@ interface ActualProps {
 }
 
 export default function ProgramItem(props: Props) {
-  return (
-    <div className="flex grid-cols-3 gap-10">
-      {/* {program.map((time, name, guest, index) => (
-        <ProgramItem key={index} name={name} index={index} />
-      ))}
-      <h3>{props.time}</h3>
-      <div className="grid-rows-2 gap-10">
-        {" "}
-        <h3>{props.name}</h3>
-        <h4 className="">{props.guest}</h4>
-        <img></img>
-      </div> */}
-    </div>
-  );
+  {
+    return program.map((programInfo) => {
+      const { time, desc, guest } = programInfo;
+      return (
+        <div>
+          <h3>{props.time}</h3>
+          <div className="">
+            <h3>{props.desc}</h3>
+            <h4 className="">{props.guest}</h4>
+          </div>
+        </div>
+      );
+    });
+  }
 }
+
+//         return
+//       })}
+// <div className="flex grid-cols-3 gap-10">
+//       <h3>{props.time}</h3>
+//       <div className="grid-rows-2 gap-10">
+//         {" "}
+//         <h3>{props.desc}</h3>
+//         <h4 className=""> {props.guest}</h4>
+//       </div>
+//     </div>
+//   );
+// }
