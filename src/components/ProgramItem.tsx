60b0fb55e7d5177c81bb9017fb088fffa41a5094
Kelly <75859041+kellyhady@users.noreply.github.com>
2023-11-06 22:58:22 -0800

    updates and init
    updated ProgramItem component
    added arrow icon as file
    started ProgramData

--- src/components/ProgramItem.tsx
+++ src/components/ProgramItem.tsx
@@ -1,18 +1,27 @@
+import program from "@/data/ProgramData";
+
 interface Props {
   time: string;
-  desc: string;
-  guest: string;
+  desc?: string;
+  guest?: string;
+}
+interface ActualProps {
+  items?: Props | Array<Props>;
 }
 
 export default function ProgramItem(props: Props) {
   return (
     <div className="flex grid-cols-3 gap-10">
+      {/* {program.map((time, name, guest, index) => (
+        <ProgramItem key={index} name={name} index={index} />
+      ))}
       <h3>{props.time}</h3>
       <div className="grid-rows-2 gap-10">
         {" "}
-        <h3>{props.desc}</h3>
-        <h4>{props.guest}</h4>
-      </div>
+        <h3>{props.name}</h3>
+        <h4 className="">{props.guest}</h4>
+        <img></img>
+      </div> */}
     </div>
   );
 }
---
+++ src/data/ProgramData.tsx
@@ -0,0 +1,19 @@
+const program = [
+  {
+    time: "08:30",
+    desc: "Doors Open",
+    guest: "",
+  },
+  {
+    time: "10:00",
+    desc: "Land Acknowledgment",
+    guest: "/partner-logos/sfu-alumni.png",
+  },
+  {
+    time: "10:20",
+    desc: ["Land Acknowledgment", "Performance"],
+    guest: ["Carleen Thomas", "M'Girl"],
+  },
+];
+
+export default program;
