bc257c108060a82bc12dcf6ccc058bfb6e00097e
Kelly <75859041+kellyhady@users.noreply.github.com>
2023-11-09 00:48:13 -0800

    update

--- src/pages/speakersInfo/Honeyman.tsx
+++ src/pages/speakersInfo/Honeyman.tsx
@@ -1,10 +1,7 @@
 // program heeader or no?
 function Honeyman() {
   return (
-    <div
-      className="grid grid-cols-2 items-center
-    "
-    >
+    <div className="grid grid-cols-2 items-center">
       <h1 className="text-pagehead font-light inline-flex">Hayley Honeyman</h1>
       <h4 className="text-caption text-ted-grey">ADHD advocate</h4>
     </div>
